39943a0937ec963819f6fd1da598b5523dc1dc66
Anwesa33 <122005103+Anwesa33@users.noreply.github.com>
2025-01-19 15:52:22 +0000

    convertion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score)); means the same thing as line above, this is a method, written inside ()
+
+let valueInNumber = Number(score) //when we convert a value in another type we have to write the typen name starting with Capital
+
+// console.log(valueInNumber);
+
+// NaN means not a number
+// when we forefully convert 33abc to number and print the vlue of it 
+// then it prnts NaN
+// true will be converted in 1 and false in 0
+// null in 0
+// undefined in NaN
+
+let isLoggedIn = "Anwesa"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
